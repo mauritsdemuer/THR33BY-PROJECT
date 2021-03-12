aa76eff595fa987575f5cba9068572781492120f
Maurits De Muer <maurits.demuer@student.ap.be>
2021-03-12 11:43:20 +0100

    onclick sorting button toegevoegd
    -Bij het klikken op "aantal bevestigen" zal het aantal weergegeven worden in het counter element
    
    -Bij NaN waarde, geef error weer.

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,26 @@
-import bootstrap from 'bootstrap'
+// place text in html elements
+let showText = function (text, id) {
+  let changeText = document.getElementById(id);
+  changeText.textContent = text;
+  return;
+};
+
+// get data from form
+function getDataFromForm(form, data) {
+  let chosenForm = document.getElementById(form);
+  let formData = new FormData(chosenForm);
+  let chosenData = formData.get(data);
+  return chosenData;
+}
+
+// sorting button
+let startSortButton = document.getElementById("startSorting");
+let counter = 0;
+startSortButton.addEventListener("click", function () {
+  counter = getDataFromForm("sortForm", "sortButton");
+  if (isNaN(counter)) {
+    alert("Dit is geen geldige waaride. Probeer opnieuw!");
+  } else {
+    showText(counter, "counter");
+  }
+});
