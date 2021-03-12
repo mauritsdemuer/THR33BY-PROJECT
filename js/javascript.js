4f7a5572e5eb8e3455209d16d11f033267ab0964
yoramLen <yoram.lenaers@student.ap.be>
2021-03-12 12:31:16 +0100

    counters gefikst

--- js/javascript.js
+++ js/javascript.js
@@ -1,33 +1,41 @@
 // place text in html elements
-let showText = function (text, id) {
-  let changeText = document.getElementById(id);
-  changeText.textContent = text;
-  return;
+let showText = function(text, id) {
+    let changeText = document.getElementById(id);
+    changeText.textContent = text;
+    return;
 };
 
 // get data from form
 function getDataFromForm(form, data) {
-  let chosenForm = document.getElementById(form);
-  let formData = new FormData(chosenForm);
-  let chosenData = formData.get(data);
-  return chosenData;
+    let chosenForm = document.getElementById(form);
+    let formData = new FormData(chosenForm);
+    let chosenData = formData.get(data);
+    return chosenData;
 }
 
 // start sorting button
 let startSortButton = document.getElementById("startSorting");
-let counter = 0;
-startSortButton.addEventListener("click", function () {
-  counter = getDataFromForm("sortForm", "sortButton");
-  if (isNaN(counter)) {
-    alert("Dit is geen geldige waarde. Probeer opnieuw!");
-  } else {
-    showText(counter, "counter");
-  }
+let counterDown = 0;
+let counterUp = 0;
+startSortButton.addEventListener("click", function() {
+    counterDown = getDataFromForm("sortForm", "sortButton");
+    if (isNaN(counterDown) || counterDown === "") {
+        alert("Dit is geen geldige waarde. Probeer opnieuw!");
+    } else {
+        showText(counterDown, "counterDown");
+        showText(counterUp, "counterUp");
+    }
 });
 
 // sort set button
 let sortSetButton = document.getElementById("sortSet");
-sortSetButton.addEventListener("click", function () {
-  counter--;
-  showText(counter, "counter");
+sortSetButton.addEventListener("click", function() {
+    counterDown--;
+    counterUp++;
+    if (counterDown < 0) { alert("U bent klaar met sorteren!") } else {
+
+        showText(counterDown, "counterDown");
+        showText(counterUp, "counterUp");
+    }
+
 });
