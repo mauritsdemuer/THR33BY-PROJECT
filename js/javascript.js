d1ebbf6c56c662056133cb5df7cb6a0ab7312c3c
Maurits De Muer <72350703+mauritsdemuer@users.noreply.github.com>
2021-03-16 17:03:23 +0100

    Stop button & bevestigen modal (#22)
    -Stop button na initialiseren van "aantal bevestigen"
    -Modal na klikken van "Stoppen met ordenen"
    -Indien ja -> refresh page
    -Indien nee -> sluiten modal & continue

--- js/javascript.js
+++ js/javascript.js
@@ -1,49 +1,45 @@
 // place text in html elements
 
-
-let showText = function(text, id) {
-    let changeText = document.getElementById(id);
-    changeText.textContent = text;
-    return;
+let showText = function (text, id) {
+  let changeText = document.getElementById(id);
+  changeText.textContent = text;
+  return;
 };
 
 // get data from form
 function getDataFromForm(form, data) {
-    let chosenForm = document.getElementById(form);
-    let formData = new FormData(chosenForm);
-    let chosenData = formData.get(data);
-    return chosenData;
+  let chosenForm = document.getElementById(form);
+  let formData = new FormData(chosenForm);
+  let chosenData = formData.get(data);
+  return chosenData;
 }
 
 // start sorting button
 let startSortButton = document.getElementById("startSorting");
 let counterDown = 0;
 let counterUp = 0;
-startSortButton.addEventListener("click", function() {
-    counterDown = getDataFromForm("sortForm", "sortButton");
-    if (isNaN(counterDown) || counterDown === "") {
-        alert("Dit is geen geldige waarde. Probeer opnieuw!");
-    } else {
-        showText(counterDown, "counterDown");
-        showText(counterUp, "counterUp");
-        $("#sortForm").addClass("d-none");
-    
-    }
+startSortButton.addEventListener("click", function () {
+  counterDown = getDataFromForm("sortForm", "sortButton");
+  if (isNaN(counterDown) || counterDown === "") {
+    alert("Dit is geen geldige waarde. Probeer opnieuw!");
+  } else {
+    showText(counterDown, "counterDown");
+    showText(counterUp, "counterUp");
+    $("#sortForm").addClass("d-none");
+    $("#stopSort").removeClass("d-none");
+  }
 });
 
 // sort set button
 let sortSetButton = document.getElementById("sortSet");
-sortSetButton.addEventListener("click", function() {
-    counterDown--;
-    counterUp++;
-    if (counterDown < 0) { 
-        alert("U bent klaar met sorteren!");
-        location.reload(); } else {
-
-        showText(counterDown, "counterDown");
-        showText(counterUp, "counterUp");
-        
-    }
-
+sortSetButton.addEventListener("click", function () {
+  counterDown--;
+  counterUp++;
+  if (counterDown < 0) {
+    alert("U bent klaar met sorteren!");
+    location.reload();
+  } else {
+    showText(counterDown, "counterDown");
+    showText(counterUp, "counterUp");
+  }
 });
-
